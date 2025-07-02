df6a8149f3a4e5e0673b4b0c467a3b82f5cea1bc
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-07-02 11:35:08 +0900

    0007 ◎ C 07/02 07/02 0.001│ToDo│上下・左右距離計算 calc_vert_dist()

--- HexGameCpp/HexGameCpp/Board.cpp
+++ HexGameCpp/HexGameCpp/Board.cpp
@@ -5,21 +5,24 @@
 
 using namespace std;
 
+#define		is_empty()	empty()
+
 Board::Board(int bd_width)
 	: m_bd_width(bd_width), m_bd_height(bd_width)
 {
 	m_ary_width = m_bd_width + 1;
 	m_ary_height = m_bd_width + 2;
 	m_ary_size = m_ary_width * m_ary_height;
-	m_board.resize(m_ary_size);
+	m_cell.resize(m_ary_size);
+	m_dist.resize(m_ary_size);
 	init();
 }
 void Board::init() {
-	fill(m_board.begin(), m_board.end(), BWALL);	//	for 上下壁
+	fill(m_cell.begin(), m_cell.end(), BWALL);	//	for 上下壁
 	for(int y = 0; y < m_bd_height; ++y) {
-		m_board[xyToIndex(-1, y)] = WWALL;
+		m_cell[xyToIndex(-1, y)] = WWALL;
 		for(int x = 0; x < m_bd_width; ++x)
-			m_board[xyToIndex(x, y)] = EMPTY;
+			m_cell[xyToIndex(x, y)] = EMPTY;
 	}
 }
 void Board::print() const {
@@ -31,7 +34,7 @@ void Board::print() const {
 		cout << string(y, ' ');
 		printf("%2d:", y+1);
 		for(int x = 0; x < m_bd_width; ++x) {
-			switch(m_board[xyToIndex(x, y)]) {
+			switch(m_cell[xyToIndex(x, y)]) {
 			case EMPTY: cout << "・"; break;
 			case BLACK: cout << "●"; break;
 			case WHITE: cout << "◯"; break;
@@ -41,4 +44,123 @@ void Board::print() const {
 		}
 		cout << endl;
 	}
+	cout << endl;
+}
+void Board::print_dist() const {
+	for(int y = 0; y < m_bd_height; ++y) {
+		cout << string(y*2, ' ');
+		printf("%2d:", y+1);
+		for(int x = 0; x < m_bd_width; ++x) {
+			auto d = m_dist[xyToIndex(x, y)];
+			if( d == USHORT_MAX ) cout << "  -1";
+			else printf("%4d", d);
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void Board::calc_dist_sub(int ix, int dix, ushort dist, byte col) {
+	if( m_cell[dix] == EMPTY && dist + 1 < m_dist[dix] ) {
+		m_dist[dix] = dist + 1;
+		m_list2.push_back(dix);
+	} else if( m_cell[dix] == col && dist < m_dist[dix] ) {
+		m_dist[dix] = dist;
+		m_list2.push_back(dix);
+	}
+}
+int Board::calc_vert_dist() {
+	fill(m_dist.begin(), m_dist.end(), USHORT_MAX);
+	m_front.clear();
+	m_list1.clear();
+	for(int x = 0; x < m_bd_width; ++x) {
+		int ix = xyToIndex(x, 0);
+		switch( m_cell[ix] ) {
+		case EMPTY:
+			m_dist[ix] = 1;
+			m_list1.push_back(ix);
+			break;
+		case BLACK:
+			m_dist[ix] = 0;
+			m_front.push_back(ix);
+			break;
+		}
+	}
+	if( m_front.is_empty() ) {
+		m_front.swap(m_list1);
+		m_list1.clear();
+	}
+	//print_dist();
+	while( !m_front.is_empty() ) {
+		m_list2.clear();
+		for(auto ix: m_front) {
+			auto dist = m_dist[ix];
+			calc_dist_sub(ix, ix - m_ary_width, dist, BLACK);
+			calc_dist_sub(ix, ix - m_ary_width + 1, dist, BLACK);
+			calc_dist_sub(ix, ix - 1, dist, BLACK);
+			calc_dist_sub(ix, ix + 1, dist, BLACK);
+			calc_dist_sub(ix, ix + m_ary_width - 1, dist, BLACK);
+			calc_dist_sub(ix, ix + m_ary_width, dist, BLACK);
+		}
+		m_front.swap(m_list2);
+		if( !m_list1.is_empty() ) {
+			m_front.insert(m_front.end(), m_list1.begin(), m_list1.end());		//	front += list1
+			m_list1.clear();
+		}
+		//print_dist();
+	}
+	ushort mind = USHORT_MAX;
+	for(int x = 0; x < m_bd_width; ++x) {
+		int ix = xyToIndex(x, m_bd_height-1);
+		mind = min(mind, m_dist[ix]);
+	}
+	return mind;
+	//auto itr = min_element(&m_cell[xyToIndex(0, m_bd_height-1)], &m_cell[xyToIndex(m_bd_width-1, m_bd_height-1)]);
+	//return *itr;
+}
+int Board::calc_horz_dist() {
+	fill(m_dist.begin(), m_dist.end(), USHORT_MAX);
+	m_front.clear();
+	m_list1.clear();
+	for(int y = 0; y < m_bd_width; ++y) {
+		int ix = xyToIndex(0, y);
+		switch( m_cell[ix] ) {
+		case EMPTY:
+			m_dist[ix] = 1;
+			m_list1.push_back(ix);
+			break;
+		case WHITE:
+			m_dist[ix] = 0;
+			m_front.push_back(ix);
+			break;
+		}
+	}
+	if( m_front.is_empty() ) {
+		m_front.swap(m_list1);
+		m_list1.clear();
+	}
+	//print_dist();
+	while( !m_front.is_empty() ) {
+		m_list2.clear();
+		for(auto ix: m_front) {
+			auto dist = m_dist[ix];
+			calc_dist_sub(ix, ix - m_ary_width, dist, WHITE);
+			calc_dist_sub(ix, ix - m_ary_width + 1, dist, WHITE);
+			calc_dist_sub(ix, ix - 1, dist, WHITE);
+			calc_dist_sub(ix, ix + 1, dist, WHITE);
+			calc_dist_sub(ix, ix + m_ary_width - 1, dist, WHITE);
+			calc_dist_sub(ix, ix + m_ary_width, dist, WHITE);
+		}
+		m_front.swap(m_list2);
+		if( !m_list1.is_empty() ) {
+			m_front.insert(m_front.end(), m_list1.begin(), m_list1.end());		//	front += list1
+			m_list1.clear();
+		}
+		//print_dist();
+	}
+	ushort mind = USHORT_MAX;
+	for(int y = 0; y < m_bd_width; ++y) {
+		int ix = xyToIndex(m_bd_width-1, y);
+		mind = min(mind, m_dist[ix]);
+	}
+	return mind;
 }
--- HexGameCpp/HexGameCpp/Board.h
+++ HexGameCpp/HexGameCpp/Board.h
@@ -3,9 +3,12 @@
 #include <vector>
 
 using byte = unsigned char;
+using ushort = unsigned short;
 
 enum {
 	EMPTY = 0, BLACK, WHITE, BWALL, WWALL,
+	BYTE_MAX = 0xff,
+	USHORT_MAX = 0xffff,
 };
 
 class Board
@@ -15,15 +18,25 @@ public:
 public:
 	void	init();
 	void	print() const;
+	void	print_dist() const;
 	int		xyToIndex(int x, int y) const { return (y+1)*m_ary_width + x; }
-	byte	get_color(int x, int y) const { return m_board[xyToIndex(x, y)]; }
-	void	set_color(int x, int y, byte col) { m_board[xyToIndex(x, y)] = col; }
+	byte	get_color(int x, int y) const { return m_cell[xyToIndex(x, y)]; }
+	void	set_color(int x, int y, byte col) { m_cell[xyToIndex(x, y)] = col; }
+
+	int		calc_vert_dist();	//	（黒）上下辺間距離計算
+	int		calc_horz_dist();	//	（白）左右辺間距離計算
+	void	calc_dist_sub(int ix, int dix, ushort dist, byte col);
+
 public:
 	int		m_bd_width;
 	int		m_bd_height;
 	int		m_ary_width;
 	int		m_ary_height;
 	int		m_ary_size;
-	std::vector<byte>	m_board;
+	std::vector<byte>	m_cell;
+	std::vector<ushort>	m_dist;
+	std::vector<byte>	m_front;
+	std::vector<byte>	m_list1;
+	std::vector<byte>	m_list2;
 };
 
