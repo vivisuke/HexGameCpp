46fbbff152e7554d9ae6d8fd1ad3f89c10843a28
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-07-03 04:01:49 +0900

    0011 ◎ C 07/03 07/03 0.001│問題│4x4 a3 にだけ黒があるときの、評価値が不正

--- HexGameCpp/HexGameCpp/HexGameCpp.cpp
+++ HexGameCpp/HexGameCpp/HexGameCpp.cpp
@@ -4,13 +4,15 @@
 
 using namespace std;
 
-const int BD_WIDTH = 3;
+const int BD_WIDTH = 4;
 
 int main()
 {
 	Board bd(BD_WIDTH);
 	if( true ) {
-		bd.set_color(2, 0, BLACK);
+		bd.init();
+		//bd.set_color(3, 1, BLACK);
+		bd.set_color(0, 2, BLACK);
 		//assert( bd.get_color(1, 1) ==  BLACK );
 		//bd.set_color(0, 2, BLACK);
 		//bd.set_color(2, 2, WHITE);
@@ -22,6 +24,21 @@ int main()
 		auto dh = bd.calc_horz_dist();
 		cout << "horizontal dist = " << dh << endl;
 		//auto ev = bd.eval(WHITE);
+	}
+	if( false ) {
+		bd.init();
+		for(int y = 0; y < BD_WIDTH; ++y) {
+			for(int x = 0; x < BD_WIDTH; ++x) {
+				bd.set_color(x, y, BLACK);
+				auto ev = bd.eval(WHITE);
+				//cout << "ev = " << ev << endl;
+				//bd.print();
+				bd.set_color(x, y, EMPTY);
+				printf("%3d", ev);
+			}
+			cout << endl;
+		}
+		bd.print();
 	}
 #if 0
 	if( false ) {
